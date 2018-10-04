e8ef71852a682a442fcf5338fc548269f9059f94
Tu Duong <duong.n.tu@gmail.com>
2018-10-03 21:44:06 -0400

    with PORT

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -40,7 +40,7 @@
 # starter code.
 #
 # After writing each step, restart the server and run test.py to test it.
-
+import os
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
@@ -149,6 +149,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write(("Do it again. Missing something AGAIN").encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
